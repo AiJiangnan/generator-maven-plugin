8ce81bc76c1226fa3c8f11e5687efb5ebf567c76
AiJiangnan <904629998@qq.com>
2018-08-02 17:23:43 +0800

    [update] 修改生成代码工厂类名

--- src/main/java/cn/codeartist/generator/maven/plugin/GeneratorMojo.java
+++ src/main/java/cn/codeartist/generator/maven/plugin/GeneratorMojo.java
@@ -1,8 +1,8 @@
 package cn.codeartist.generator.maven.plugin;
 
-import cn.codeartist.code.generator.session.GenSession;
-import cn.codeartist.code.generator.session.GenSessionFactory;
-import cn.codeartist.code.generator.session.defaults.BaseGenSessionFactory;
+import cn.codeartist.code.generator.session.GeneratorSession;
+import cn.codeartist.code.generator.session.GeneratorSessionFactory;
+import cn.codeartist.code.generator.session.defaults.BaseGeneratorSessionFactory;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -22,8 +22,8 @@ public class GeneratorMojo extends AbstractMojo {
         }
         getLog().info("配置文件路径: " + configFile.getName());
         getLog().info("开始生成源代码......");
-        GenSessionFactory factory = new BaseGenSessionFactory().build(configFile);
-        GenSession session = factory.openSession();
+        GeneratorSessionFactory factory = new BaseGeneratorSessionFactory().build(configFile);
+        GeneratorSession session = factory.openSession();
         session.generate();
         getLog().info("生成源代码完成");
     }
